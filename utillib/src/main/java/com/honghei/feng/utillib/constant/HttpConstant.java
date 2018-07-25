0fb5048d3c939d5e68858e5b6ca2dd85e4ab62fb
feng <fengchanghao@namibox.com>
2018-07-25 19:49:13 +0800

    添加异常处理器

---
+++ utillib/src/main/java/com/honghei/feng/utillib/constant/HttpConstant.java
@@ -0,0 +1,11 @@
+package com.honghei.feng.utillib.constant;
+
+/**
+ * author : feng
+ * description ：http常量
+ * creation time : 18-7-25下午6:30
+ */
+public class HttpConstant {
+
+  public static final int VALID_RESPONSE = 200;
+}
