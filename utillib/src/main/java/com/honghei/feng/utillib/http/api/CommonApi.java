c285aa68ed5e9b154570cf1b5e5c5aeefd21857a
feng <fengchanghao@namibox.com>
2018-07-31 18:13:25 +0800

    添加图文混传的方法

--- utillib/src/main/java/com/honghei/feng/utillib/http/api/CommonApi.java
+++ utillib/src/main/java/com/honghei/feng/utillib/http/api/CommonApi.java
@@ -3,10 +3,12 @@ package com.honghei.feng.utillib.http.api;
 import com.google.gson.JsonElement;
 import io.reactivex.Observable;
 import java.util.Map;
+import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Url;
 
@@ -20,10 +22,23 @@ public interface CommonApi {
   @GET
   Observable<HttpResponse> commonGet(@Url String url);
 
+  /**
+   * 上传json字符串
+   */
   @POST
   Observable<HttpResponse> commonPost(@Url String url, @Body JsonElement jsonElement);
 
+  /**
+   * 上传表单
+   */
   @FormUrlEncoded
   @POST
   Observable<HttpResponse> commonPost(@Url String url, @FieldMap Map<String, String> field);
+
+  /**
+   * 图文混传
+   */
+  @Multipart
+  @POST
+  Observable<HttpResponse> commonPost(@Url String url, @Body RequestBody requestBody);
 }
